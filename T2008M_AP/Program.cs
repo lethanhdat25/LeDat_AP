89f6d6877b82884582a52a642a8bd15665f30443
lethanhdat25 <70951430+lethanhdat25@users.noreply.github.com>
2021-05-30 23:25:32 +0700

    uo

---
+++ T2008M_AP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace T2008M_AP
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+            Console.WriteLine("Nhap mot so");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int x1=0, x2=1, x3=0;
+            while (x3 < n)
+            {
+                
+                x3 = x2 + x1;
+                x1 = x2;
+                x2 = x3;
+            }
+
+            Console.WriteLine(x1);
+        }
+
+        
+    }
+}
