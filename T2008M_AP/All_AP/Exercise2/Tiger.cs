3ce6392b9950ebe5fff6552853f6f60f1978b5e9
Admin <isaacbest.ig@gmail.com>
2021-06-07 11:08:18 +0700

    up

---
+++ T2008M_AP/All_AP/Exercise2/Tiger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace T2008M_AP.All_AP.Exercise2
+{
+    public class Tiger : Animal
+    {
+        
+        public override void Show()
+        {
+            Console.WriteLine("Name: "+base.Name+", Weight: "+base.Weight);
+        }
+
+        public override void SetMe(int weight, string name)
+        {
+            base.Weight = weight;
+            base.Name = name;
+        }
+    }
+}
